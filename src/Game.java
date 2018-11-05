060d040326818a5e59a8510724c90e5c3fc04337
linaeb <lina@linabe.se>
2018-11-05 10:45:13 +0100

    walls working

---
+++ src/Game.java
@@ -0,0 +1,17 @@
+/*
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.nio.charset.Charset;
+
+public class Game {
+    Terminal terminal;
+
+    public Game() {
+        this.terminal = DefaultTerminalFactory
+        terminalFactory = new DefaultTerminalFactory(System.out, System.in, Charset.forName("UTF8"));
+        Terminal terminal = terminalFactory.createTerminal();
+    }
+    }
+}
+*/
